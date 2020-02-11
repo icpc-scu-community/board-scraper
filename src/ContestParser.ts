37999c865b51b70e4ac02b0ffa911c23d33afe76
Kerollos Magdy <kerolloz@yahoo.com>
2020-02-11 22:28:24 +0200

    feat: handle pages with pending submissions
    fixes #1

--- src/ContestParser.ts
+++ src/ContestParser.ts
@@ -1,6 +1,8 @@
 import { Browser } from "puppeteer";
 import { Submission, Contest } from "./models";
 
+type sub_t = { [key: string]: string };
+
 export class ContestParser {
   private _main_link: string;
 
@@ -32,43 +34,52 @@ export class ContestParser {
     return lastPage;
   }
 
-  private async parsePage(page_number: number) {
+  /**
+  * parses the page and returns whether it has pendening submissions
+  */
+  private async parsePage(page_number: number): Promise<boolean> {
     console.log(`\t@page #${page_number}`)
     const page = await this._browser.newPage();
     await page.goto(
       this.formulateLink(page_number),
       { waitUntil: "domcontentloaded" }
     );
+
+    let submissions: sub_t[] = [];
+    let containsPendingSubmissions = false;
     try {
-      const submissions = await page.evaluate(() => {
-        const rows = document.querySelectorAll(
-          ".status-frame-datatable > tbody tr:not(:first-of-type)"
-        );
+      ({ submissions, containsPendingSubmissions } = await page.evaluate(() => {
+        const SUBMISSIONS_TABLE_SELECTOR = ".status-frame-datatable > tbody tr:not(:first-of-type)";
+        const containsPendingSubmissions = !!document.querySelector(`${SUBMISSIONS_TABLE_SELECTOR} > td[waiting=true]`)
+        const rows = document.querySelectorAll(SUBMISSIONS_TABLE_SELECTOR);
         const attrs = ["id", "date", "name", "problem", "lang", "verdict", "time", "memory",];
-        const output: any[] = [];
-        rows.forEach(row => {
+
+        const submissions = containsPendingSubmissions ? [] : Array.from(rows).map(row => {
           const columns = row.querySelectorAll("td");
-          const submission: { [key: string]: string } = {};
+          const submission: sub_t = {};
           for (let i = 0; i < attrs.length; i++) {
             const attr_name = attrs[i];
             submission[attr_name] = columns[i].innerText;
           }
-          output.push(submission);
+          return submission;
         });
-        return output;
-      });
-      submissions.forEach(async (submission) => {
-        submission['contest_id'] = this._codeforces_contest_id;
-        const sub = new Submission(submission);
-        try {
-          await sub.save();
-        } catch (err) { console.log(err.errmsg) };
-      })
+        return { submissions, containsPendingSubmissions }
 
+      }));
     } catch (error) {
       console.error(error);
     }
+
+
     page.close();
+    submissions.forEach(async (submission) => {
+      submission['contest_id'] = this._codeforces_contest_id;
+      const sub = new Submission(submission);
+      try {
+        await sub.save();
+      } catch (err) { console.log(err.errmsg) };
+    })
+    return containsPendingSubmissions;
   }
 
   async parseAll() {
@@ -90,7 +101,11 @@ export class ContestParser {
     }
     console.log(`\tStarting from: ${pageNumber}, to: ${endPage}`);
     for (; pageNumber <= endPage; pageNumber++) {
-      await this.parsePage(pageNumber);
+      const hasPendingSubmissions = await this.parsePage(pageNumber);
+      if (hasPendingSubmissions) {
+        console.log("\tPausing parsing because of pending submissions!");
+        break;
+      }
       contest_doc.set('lastParsedPage', pageNumber);
       await contest_doc.save();
     }
