9d518b10fcaa646fd1b6b0db13e861d99d1b0ead
Amr Salama <amr.mo.salama@gmail.com>
2021-10-20 01:06:50 +0200

    feat(config): introduce env utility

---
+++ src/config/env.ts
@@ -0,0 +1,14 @@
+import Logger from '../utils/Logger';
+
+function env(name: string, required?: true): string;
+function env(name: string, required: false): string | undefined;
+function env(name: string, required = true): string | undefined {
+  const envVar = process.env[name];
+  if (required && envVar === undefined) {
+    Logger.fail(`Environement variable "${name}" is required but not set.`);
+    process.exit(1);
+  }
+  return envVar;
+}
+
+export default env;
