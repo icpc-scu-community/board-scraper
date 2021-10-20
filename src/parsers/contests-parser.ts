b4110181cba040bcd7977962e3582487df433d2d
Amr Salama <amr.mo.salama@gmail.com>
2021-10-21 01:59:10 +0200

    feat(parsers): initial implement of contests parser

--- src/parsers/contests-parser.ts
+++ src/parsers/contests-parser.ts
@@ -1,7 +1,38 @@
 import { openMongooseConnection, closeMongooseConnection } from '../database/mongoose-connection';
-import { mongoURIEnvVar } from '../config';
+import { mongoURIEnvVar, contestsEnvVar } from '../config';
+import { Logger, crawl, contestPageUrl } from '../utils';
+import { Contest } from '../database/models';
 
 (async () => {
+  // parse
+  const contestsParsing = contestsEnvVar.map(({ contestId, groupId }) => parseContest(contestId, groupId));
+  const contests = await Promise.all(contestsParsing);
+
+  // save
   await openMongooseConnection(mongoURIEnvVar);
+  await Contest.insertMany(contests, { ordered: false });
   await closeMongooseConnection();
 })();
+
+async function parseContest(contestId: string, groupId: string) {
+  const logEvent = `${groupId}/${contestId}`;
+  Logger.log(logEvent, `Parsing problems in group "${groupId}", contest "${contestId}"`);
+
+  const $ = await crawl(contestPageUrl(contestId, groupId));
+  const problems = $('.problems')
+    .find('tr:not(:first-child)')
+    .map((_, el) => ({
+      id: $(el).find('td:nth-child(1) a').text().trim(),
+      name: $(el).find('td:nth-child(2) a').text().trim(),
+    }))
+    .get();
+  const name = $('#sidebar div:nth-child(3) a').text().trim();
+  Logger.success(logEvent);
+
+  return {
+    id: contestId,
+    groupId,
+    name,
+    problems,
+  };
+}
