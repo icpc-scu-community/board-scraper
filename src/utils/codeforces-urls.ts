51fb52110026f6ebdad7bfafafb7699e471f0785
Amr Salama <amr.mo.salama@gmail.com>
2021-10-24 01:54:50 +0200

    refactor(utils): remove unused utils

--- src/utils/codeforces-urls.ts
+++
@@ -1,7 +0,0 @@
-export const contestPageUrl = (contestId: string, groupId: string): string => {
-  return `https://codeforces.com/group/${groupId}/contest/${contestId}`;
-};
-
-export const statusPageUrl = (contestId: string, groupId: string, page = 1): string => {
-  return `https://codeforces.com/group/${groupId}/contest/${contestId}/status/page/${page}?order=BY_ARRIVED_ASC`;
-};
--- src/utils/crawler.ts
+++
@@ -1,7 +0,0 @@
-import cheerio from 'cheerio';
-import got from 'got';
-
-export async function crawl(url: string): Promise<cheerio.Root> {
-  const response = await got(url);
-  return cheerio.load(response.body);
-}
