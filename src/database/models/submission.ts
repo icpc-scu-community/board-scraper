a41344cd339809523e4a71c96a513c15bd891dc6
Amr Salama <amr.mo.salama@gmail.com>
2021-10-23 20:42:12 +0200

    feat(models): add `isAc` to submissions

--- src/database/models/submission.ts
+++ src/database/models/submission.ts
@@ -1,5 +1,10 @@
-import { prop, getModelForClass, DocumentType } from '@typegoose/typegoose';
+import { prop, getModelForClass, pre, DocumentType } from '@typegoose/typegoose';
 
+const ACCEPTED_VERDICT = 'Accepted';
+
+@pre<Submission>('validate', function () {
+  this.isAc = this.verdict === ACCEPTED_VERDICT;
+})
 class Submission {
   @prop({ required: true, unique: true })
   public id!: number;
@@ -12,6 +17,9 @@ class Submission {
 
   @prop({ required: true })
   public verdict!: string;
+
+  @prop({ required: true })
+  public isAc!: boolean;
 
   @prop({ required: true, index: true })
   public contestId!: number;
