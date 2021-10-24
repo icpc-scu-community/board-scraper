e33cef21df436b1e21c3a6fcc67aeb362abfa0f3
Amr Salama <amr.mo.salama@gmail.com>
2021-10-24 14:21:25 +0200

    refactor(index): update index messages

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,31 @@
-import { mongoURIEnvVar, contestsEnvVar } from './config';
+import { mongoURIEnvVar } from './config';
 import { openMongooseConnection, closeMongooseConnection } from './database/mongoose';
 import { ScraperModel } from './database/models';
+import { Logger } from './services/logger';
+import { formatTime } from './utils';
 
 (async () => {
+  // log start time
+  const startTime = new Date();
+  Logger.success(`Scraper started at ${startTime.toLocaleTimeString()}.`);
+
+  // db connection start
   await openMongooseConnection(mongoURIEnvVar);
 
-  const startTime = new Date();
-  console.log(`[ 🛫 ] Scraper Started ${startTime.toLocaleTimeString()}`);
+  // parsing goes here
   const lastUpdate = (await ScraperModel.findOne())?.lastUpdate;
   const lastUpdatedDate = lastUpdate ? new Date(lastUpdate).toLocaleString() : 'N/A - First run';
-  console.log(`[ 🌀 ] Last Updated ${lastUpdatedDate}`);
-  console.log(`[ ❗ ] Parsing ${contestsEnvVar.length} contest(s)`);
+  Logger.success(`Last Updated ${lastUpdatedDate}`);
 
+  // update last update time
   await new ScraperModel().save();
 
+  // db connection end
+  await closeMongooseConnection();
+
+  // log end time
   const endTime = new Date();
   const takenTime = endTime.getTime() - startTime.getTime();
-  console.log(`[ 🛬 ] Scraper Finished ${endTime.toLocaleTimeString()}`);
-  console.log(`[ 🕑 ] Took ${calculateTime(takenTime)} ~ Total added ${'0'}`);
-
-  await closeMongooseConnection();
+  Logger.success(`Scraper finished at ${endTime.toLocaleTimeString()}.`);
+  Logger.success(`Took ${formatTime(takenTime)} ~ Total added ${'0'}.`);
 })();
-
-function calculateTime(distance: number) {
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  return `${hours}h ${minutes}m ${seconds}s`;
-}
