ab4f468b2e1907a468d1412b8864f513a8b80d9f
Amr Salama <amr.mo.salama@gmail.com>
2021-10-24 14:21:25 +0200

    refactor(index): update index messages

---
+++ src/utils/time-formatter.ts
@@ -0,0 +1,6 @@
+export function formatTime(time: number): string {
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((time % (1000 * 60)) / 1000);
+  return `${hours}h ${minutes}m ${seconds}s`;
+}
