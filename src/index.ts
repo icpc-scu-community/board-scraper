b3dc597a4905065f669e36167e1c9cdaf7d1a515
Amr Salama <amr.mo.salama@gmail.com>
2021-10-24 16:42:22 +0200

    refactor(index): improve promises sequence

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,23 @@
-import { mongoURIEnvVar } from './config';
 import { openMongooseConnection, closeMongooseConnection } from './database/mongoose';
 import { ScraperModel } from './database/models';
-import { Logger } from './services/logger';
+import { mongoURIEnvVar } from './config';
 import { formatTime } from './utils';
+import { Logger } from './services/logger';
 import { parseContests, parseSubmissions } from './parsers';
 
 (async () => {
-  // log start time
   const startTime = new Date();
   Logger.success(`Scraper started at ${startTime.toLocaleTimeString()}.`);
 
-  // db connection start
-  await openMongooseConnection(mongoURIEnvVar);
-
-  // last update
-  const lastUpdate = (await ScraperModel.findOne())?.lastUpdate;
-  const lastUpdatedDate = lastUpdate ? new Date(lastUpdate).toLocaleString() : 'N/A - First run';
-  Logger.success(`Last Updated ${lastUpdatedDate}`);
-
-  // parse
-  await parseContests();
-  await parseSubmissions();
-
-  // update last update time
-  await new ScraperModel().save();
-
-  // db connection end
-  await closeMongooseConnection();
-
-  // log end time
-  const endTime = new Date();
-  const takenTime = endTime.getTime() - startTime.getTime();
-  Logger.success(`Scraper finished at ${endTime.toLocaleTimeString()}.`);
-  Logger.success(`Took ${formatTime(takenTime)} ~ Total added ${'0'}.`);
+  openMongooseConnection(mongoURIEnvVar)
+    .then(() => parseContests())
+    .then(() => parseSubmissions())
+    .finally(() => ScraperModel.create({}))
+    .finally(() => closeMongooseConnection())
+    .finally(() => {
+      const endTime = new Date();
+      const takenTime = endTime.getTime() - startTime.getTime();
+      Logger.success(`Scraper finished at ${endTime.toLocaleTimeString()}.`);
+      Logger.success(`Took ${formatTime(takenTime)} ~ Total added ${'0'}.`);
+    });
 })();
